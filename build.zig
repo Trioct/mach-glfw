1715e0121f4c141a1311ff0962c67f10c6422636
Trioct <dbaumgartner21@gmail.com>
2024-05-31 21:32:13 -0500

    update for new zig version

--- build.zig
+++ build.zig
@@ -8,13 +8,13 @@ pub fn build(b: *std.Build) !void {
     var module = b.addModule("mach-glfw", .{
         .target = target,
         .optimize = optimize,
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
     const test_step = b.step("test", "Run library tests");
     const main_tests = b.addTest(.{
         .name = "glfw-tests",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -26,15 +26,15 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     })) |dep| {
         module.linkLibrary(dep.artifact("glfw"));
-        @import("glfw").addPaths(module);
+        // @import("glfw").addPaths(module);
         main_tests.linkLibrary(dep.artifact("glfw"));
-        @import("glfw").addPaths(&main_tests.root_module);
+        // @import("glfw").addPaths(&main_tests.root_module);
     }
 }
 
-comptime {
-    const supported_zig = std.SemanticVersion.parse("0.12.0-dev.3180+83e578a18") catch unreachable;
-    if (builtin.zig_version.order(supported_zig) != .eq) {
-        @compileError(std.fmt.comptimePrint("unsupported Zig version ({}). Required Zig version 2024.3.0-mach: https://machengine.org/about/nominated-zig/#202430-mach", .{builtin.zig_version}));
-    }
-}
+// comptime {
+//     const supported_zig = std.SemanticVersion.parse("0.12.0-dev.3180+83e578a18") catch unreachable;
+//     if (builtin.zig_version.order(supported_zig) != .eq) {
+//         @compileError(std.fmt.comptimePrint("unsupported Zig version ({}). Required Zig version 2024.3.0-mach: https://machengine.org/about/nominated-zig/#202430-mach", .{builtin.zig_version}));
+//     }
+// }
